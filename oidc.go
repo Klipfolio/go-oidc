68f6b5a03326fcb79d68cb2f9d67464453084161
Kyle Campbell <kcampbell@klipfolio.com>
2020-05-14 10:15:39 -0400

    Hack for cognito

--- oidc.go
+++ oidc.go
@@ -187,7 +187,7 @@ type UserInfo struct {
 	Subject       string `json:"sub"`
 	Profile       string `json:"profile"`
 	Email         string `json:"email"`
-	EmailVerified bool   `json:"email_verified"`
+	EmailVerified string `json:"email_verified"`
 
 	claims []byte
 }
